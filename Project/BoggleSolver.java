14ea3d5d9c0ce19f38f8bcac1fc042e78a866a42
lakshmireddy999 <narasareddym999@gmail.com>
2021-07-25 19:42:29 +0530

    Boggle game solver

--- Project/BoggleSolver.java
+++ Project/BoggleSolver.java
@@ -1,6 +1,8 @@
 package DataStructures;
 import java.io.*;
 import java.util.*;
+
+//Structure of Trie
 class TrieNode
 {
     char data;
@@ -15,14 +17,17 @@ class TrieNode
     }
 }
 public class BoggleSolver {
+    //Hashset to store valid words
     HashSet<String> words=new HashSet<>();
     TrieNode root;
 
+    //Initializing the root of the trie with null
     BoggleSolver()
     {
         root=new TrieNode('\0');
     }
 
+    //inserting into the trie
     void insert(String word)
     {
         TrieNode curr=root;
@@ -34,6 +39,7 @@ public class BoggleSolver {
 
             curr=curr.child[c-'a'];
         }
+        //make word as true if we reach the end of the word
         curr.isWord=true;
 
     }
@@ -41,8 +47,11 @@ public class BoggleSolver {
     {
         int m=boggle.length;
         int n=boggle[0].length;
+
+        //creating a visited array to check what all values we have traversed in the matrix
         boolean[][] visited =new boolean[m][n];
-        String str = "";
+
+        String str = "";   //empty string to store the words created from boggle
 
         // traverse all matrix elements
         for (int i = 0; i < m; i++)
@@ -53,14 +62,26 @@ public class BoggleSolver {
             }
         }
     }
+
+    /**
+     * @param boggle A 2d matrix of characters
+     * @param visited A 2d matrix to take care whether a given index is visited or not
+     * @param i current index
+     * @param j current index
+     * @param str String to store words formed from boggle
+     * @param m row length of boggle matrix
+     * @param n column length of boggle matrix
+     */
     void findWordsUtil(char[][] boggle, boolean[][] visited, int i, int j, String str, int m, int n)
     {
-        visited[i][j]=true;
-        str+=boggle[i][j];
-        if(search(str))
+        visited[i][j]=true;  //making current element of matrix as visited
+        str+=boggle[i][j];   //appending the current character from the boggle to string
+
+        if(search(str))  //searching the word on the trie if present word is valid else not valid
         {
-            words.add(str);
+            words.add(str);   //storing valid words into the hashset
         }
+
         for(int row=i-1; row<=i+1 && row<m ; row++)
         {
             for (int col = j - 1; col <= j + 1 && col < n; col++)
@@ -70,6 +91,7 @@ public class BoggleSolver {
             }
         }
 
+        //making the current index as not visited since we need this index for the next iteration
         visited[i][j]=false;
     }
     //searching in the Trie
@@ -95,8 +117,10 @@ public class BoggleSolver {
     {
         try
         {
+            //using FileReader to read the file mydictionary.txt
             FileReader fr=new FileReader("mydictionary.txt");
             Scanner sc=new Scanner(fr);
+
             while (sc.hasNextLine())
             {
                 String s=sc.nextLine();
@@ -111,11 +135,11 @@ public class BoggleSolver {
     }
     static int getIndex(String s)
     {
-        return  (int)(s.length()*Math.random());
+        return  (int)(s.length()*Math.random()); //it returns random index from a string of 26 alphabets
     }
     static void generateBoggle(char [][]boggle)
     {
-        String s="abcdefghijklmnopqrstuvwxyz";
+        String s="abcdefghijklmnopqrstuvwxyz";  //random string of 26 alphabets used to generate boggle randomly
         for (int i=0; i<4; i++)
         {
             for(int j=0; j<4; j++)
@@ -133,11 +157,21 @@ public class BoggleSolver {
 
         //random boggle board generator
         generateBoggle(boggle);
-	
-	//finding valid words from the boggle
+
+        //finding valid words from the boggle
         boggleSolver.findWords(boggle);
 
-        System.out.println(boggleSolver.words);
+        //printing all the valid words
+        System.out.println("Valid words");
+
+        Iterator i=boggleSolver.words.iterator(); //using an iterator to traverse the hashset
+
+        while (i.hasNext())
+        {
+            System.out.print(i.next()+" "); //printing all valid words
+        }
+        System.out.println();
+       // System.out.println(boggleSolver.words);
         System.out.println("Points:"+boggleSolver.words.size());
 
     }
